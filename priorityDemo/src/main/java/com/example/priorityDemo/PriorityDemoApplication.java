0a3d7a3fd26b145b7759024310b176ce1c815bec
Saurav7192 <sauravkumar71920@gmail.com>
2021-01-12 22:08:22 +0530

    imported

---
+++ priorityDemo/src/main/java/com/example/priorityDemo/PriorityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.priorityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorityDemoApplication.class, args);
+	}
+
+}
---
+++ priorityDemo/src/test/java/com/example/priorityDemo/PriorityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priorityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
